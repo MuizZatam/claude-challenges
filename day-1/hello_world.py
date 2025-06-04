3bf05ae4c3370947968466614b661aa2c0b7cb10
Muiz Zatam <muizzatam110@gmail.com>
2025-06-04 15:04:39 +0530

    Solve 1.1

---
+++ day-1/hello_world.py
@@ -0,0 +1,16 @@
+# Using std print function
+print("Hello, World!")
+
+# Using f-strings
+my_str = "Hello, World!"
+print(f"{my_str}")
+
+# Using the format method
+print("{}, {}{}".format("Hello", "World", "!"))
+
+# Using % formatting
+# Note: Method not recommended
+print("%s, %s%s" % ("Hello", "World", "!"))
+
+# Using str concatenation
+print("Hello, " + "World" + "!")
